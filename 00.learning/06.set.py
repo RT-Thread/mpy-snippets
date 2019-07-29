ad66c8dda6cd4c10eb62f705e8334a98cc1703cd
SummerGift <SummerGift@qq.com>
2019-07-29 13:52:31 +0800

    [update] set example

--- 00.learning/06.set.py
+++ 00.learning/06.set.py
@@ -14,13 +14,13 @@
 
 student = {'Tom', 'Jim', 'Mary', 'Tom', 'Jack', 'Rose'}
 print(student)   # 输出集合，重复的元素被自动去掉
- 
+
 # 成员测试
-if 'Rose' in student :
-    print('Rose 在集合中')
-else :
-    print('Rose 不在集合中')
- 
+if 'Rose' in student:
+    print('Rose is in set')
+else:
+    print('Rose is not in set')
+
 # set可以进行集合运算
 a = set('abracadabra')
 b = set('alacazam')
