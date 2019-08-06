27a643dff4861fc75ecb5ba7fb44e07691319637
SummerGift <SummerGift@qq.com>
2019-08-06 10:15:52 +0800

    [optimize] _thread_example.py

--- 01.basic/_thread_example.py
+++ 01.basic/_thread_example.py
@@ -8,9 +8,9 @@
 # 2019-06-13     SummerGift   first version
 #
 
-import _thread
-import utime as time
 import gc
+import _thread
+
 
 def testThread():
     count = 0
@@ -21,5 +21,6 @@ def testThread():
     print("Thread exit!")
     gc.collect()    # Free the memory space requested by the thread
 
+
 # TestThread thread is created with an empty argument
-_thread.start_new_thread(testThread, ())  
+_thread.start_new_thread(testThread, ())
