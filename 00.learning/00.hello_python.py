124490d2a28aafbf6507008a194ec7c463c35416
SummerGift <SummerGift@qq.com>
2019-08-06 15:37:33 +0800

    [update] hello_python

--- 00.learning/00.hello_python.py
+++ 00.learning/00.hello_python.py
@@ -1,8 +1,8 @@
-# 
+#
 # Copyright (c) 2006-2019, RT-Thread Development Team
-# 
+#
 # SPDX-License-Identifier: MIT License
-# 
+#
 # Change Logs:
 # Date           Author       Notes
 # 2019-07-27     SummerGift   first version
@@ -10,4 +10,3 @@
 
 # 第一个 Python 程序，来和 Python 打个招呼吧
 print("Hello, Python!")
-
