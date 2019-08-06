5192a3b9f2881a2b1728febef8987331a2947469
SummerGift <SummerGift@qq.com>
2019-08-06 10:49:31 +0800

    [optimize] learning examples

--- 00.learning/07.dict.py
+++ 00.learning/07.dict.py
@@ -16,12 +16,12 @@
 
 dict = {}
 dict['one'] = "RT-THREAD"
-dict[2]     = "MicroPython"
+dict[2] = "MicroPython"
 
 tinydict = {'name': 'rt-thread', 'code': 512, 'site': 'www.rt-thread.org'}
 
-print (dict['one'])       # 输出键为 'one' 的值
-print (dict[2])           # 输出键为 2 的值
-print (tinydict)          # 输出完整的字典
-print (tinydict.keys())   # 输出所有键
-print (tinydict.values()) # 输出所有值
+print(dict['one'])       # 输出键为 'one' 的值
+print(dict[2])           # 输出键为 2 的值
+print(tinydict)          # 输出完整的字典
+print(tinydict.keys())   # 输出所有键
+print(tinydict.values())  # 输出所有值
