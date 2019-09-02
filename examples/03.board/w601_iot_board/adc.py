6f066eed3f79e222564f6b09fb77bd82fb8e8927
chenyong <1521761801@qq.com>
2019-09-02 15:45:25 +0800

    【完善】W600 设备 ADC、PWM、WDT 设备例程
    Signed-off-by: chenyong <1521761801@qq.com>

--- examples/03.board/w601_iot_board/adc.py
+++ examples/03.board/w601_iot_board/adc.py
@@ -10,7 +10,8 @@
 
 from machine import ADC     # Import the ADC class from machine
 
-adc = ADC(1, 13)            # Creates an ADC object that currently uses the 13 channels of an ADC device numbered 1
-print(adc.read())           # Gets the ADC object sampling value, value range 0 to 4096
+adc = ADC("adc", 5)         # Creates an ADC object that currently uses the 5 channel(PB23) of an ADC device name "adc"
+value = (adc.read() - 8192.0) / 8192 * 2.25 / 1.2 + 1.584  # Gets the ADC object sampling value and change to voltage value
+print("Voltage Value: %.3f" % value)                       # print voltage value
 adc.deinit()                # Close ADC object
-adc.init(13)                # Open and reconfigure the ADC object
+adc.init(5)                 # Open and reconfigure the ADC object
--- examples/03.board/w601_iot_board/wdt.py
+++ examples/03.board/w601_iot_board/wdt.py
@@ -10,7 +10,7 @@
 
 from machine import WDT
 
-wdt = WDT(10)                # Create an WDT device object, set the timeout to 10 seconds
+wdt = WDT("wdg", 10)         # Create an WDT device object, set the timeout to 10 seconds
 wdt.feed()                   # Perform the "feed dog" operation to clear the watchdog device count during the timout period
                              # If not executed, the system will restart after the timeout
 print("reset system after 10 seconds")
