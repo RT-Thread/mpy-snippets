ab613e93189aa55a1e662bc278fd67a670f28cac
SummerGift <SummerGift@qq.com>
2019-09-11 17:17:48 +0800

    [optimize] weathershow demo

--- demo/weather_show/main.py
+++ demo/weather_show/main.py
@@ -3,16 +3,18 @@ import network
 import ujson as json
 from machine import LCD
 from lcd_bmp import lcd_bmp_show
+from lcd_bmp import show_image_file
 import urequests as requests
 
 def lcd_init():
     lcd = LCD()                               # Create a LCD object
     lcd.light(True)                           # Open the backlight
     lcd.set_color(lcd.WHITE, lcd.BLACK)       # Set background color and foreground color
-    lcd.fill(lcd.WHITE)                     # Fill the entire LCD with white
+    lcd.fill(lcd.WHITE)                       # Fill the entire LCD with white
     lcd.text("Weather Show", 26, 48, 32)      # prints the string at 32 font size at position (0, 48)
     lcd.text("City ShangHai", 20, 100, 32)    # prints the string at 32 font size at position (0, 48)
-    lcd_bmp_show(lcd, 40, 220, "weather.bmp")
+    show_image_file(lcd, 40, 150, 152, 48, "weather.img")
+    show_image_file(lcd, 5, 210, 230, 29, "rt_thread_micropython.img")
     return lcd
 
 def wifi_connect():
@@ -29,7 +31,6 @@ def wifi_connect():
     time.sleep(3)  # waitting to get IP
 
 def main():
-
     lcd = lcd_init()
     lcd.text("wifi connecting...", 10, 0, 24) 
     lcd.line(0, 25, 239, 25)
@@ -46,5 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
