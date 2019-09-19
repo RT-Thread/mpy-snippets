622684fa961ae15990e2eae853515722f95b6dfd
SummerGift <SummerGift@qq.com>
2019-09-19 11:02:17 +0800

    [update] bmp library

--- library/lcd_image/show_bmp_example.py
+++ library/lcd_image/show_bmp_example.py
@@ -1,6 +1,4 @@
 from machine import LCD
-from lcd_bmp import lcd_bmp_show
-import gc
 
 # Currently only supports 32-bit BMP true color image display
 
@@ -8,7 +6,7 @@ def main():
     lcd = LCD()
     lcd.light(True)
     lcd.set_color(lcd.WHITE, lcd.BLACK)
-    lcd_bmp_show(lcd, 180, 50, "sun.bmp")
+    lcd.show_bmp(180, 50, "sun.bmp")
 
 if __name__ == '__main__':
     main()
