61678b2d10dadc994b3cf7e49ac17c6df40d5679
SummerGift <SummerGift@qq.com>
2019-10-11 13:59:10 +0800

    [update] examples

--- examples/03.board/4.esp32/blink.py
+++ examples/03.board/4.esp32/blink.py
@@ -13,7 +13,7 @@ from machine import Pin
 led = Pin(2, Pin.OUT)  # create LED object from pin2,Set Pin2 to output
 
 while True:
-  led.value(1)  # Set led turn on
-  time.sleep(0.5)
-  led.value(0)  # Set led turn off
-  time.sleep(0.5)
+    led.value(1)  # Set led turn on
+    time.sleep(0.5)
+    led.value(0)  # Set led turn off
+    time.sleep(0.5)
