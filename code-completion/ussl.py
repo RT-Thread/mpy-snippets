80a88b19c23f3734c2463d6630998cfcc60d4164
Walkline <walkline@163.com>
2019-11-27 23:43:22 +0800

    append ureqeusts and ussl auto-completion

---
+++ code-completion/ussl.py
@@ -0,0 +1,16 @@
+CERT_NONE = ...
+CERT_OPTIONAL = ...
+CERT_REQUIRED = ...
+
+def wrap_socket(sock, server_side=False, keyfile=None, certfile=None, cert_reqs=CERT_NONE, ca_certs=None):
+    """
+    Wrap socket
+
+    - sock
+    - server_side
+    - keyfile
+    - certfile
+    - cert_reqs
+    - ca_certs
+    """
+    ...
